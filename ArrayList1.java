7dead547608988c4f306baf9a104be953f182e53
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-04-18 17:36:25 +0530

    Initial commit

---
+++ ArrayList1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ArrayList1
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> U=new ArrayList<String>();
+        U.add("abc");
+        U.add("dfg");
+        U.add("hij");
+        System.out.println(U);
+        U.remove(1);
+        System.out.println(U);
+        System.out.println(U.get(0));
+    }
+}
