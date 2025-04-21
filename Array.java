69dd5789bbf3c99cce0ddf7797facae09dffd9c2
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-04-21 15:55:57 +0530

    Initial commit

---
+++ Array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Array {
+    public static void main(String[] args) {
+        Double abc[] = new Double[5];
+        abc[0] = 38.9;
+        abc[1] = 4.0; 
+        abc[3] = 6.0;
+
+        System.out.println("abc[0]: " + abc[0]);  
+        System.out.println("abc[2]: " + abc[2]);  
+    }
+}
---
+++ DynamicArray.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class DynamicArray
+{
+    public static void main(String[] args)
+    {
+        int arrya[]={1,3,8,92,49};
+        System.out.println(arrya[3]);
+
+    }
+}
