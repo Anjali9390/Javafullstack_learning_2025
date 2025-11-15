8fe710037c7270f63184d229b1254864dfe795aa
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-11-15 12:12:47 +0530

    using updated version of for each loop to  printing thr names

---
+++ PrintNames.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class PrintNames
+{
+ public static void main(String[]args)
+ {
+    String[] names={"Anju","Prami","Raji","Mammu"};
+
+    for(String name:names)
+    {
+        System.out.println(name);
+    }
+ }
+}
