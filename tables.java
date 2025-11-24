958cc69805c3bd582df86f4514f92b7c6880cf91
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-11-24 09:51:39 +0530

    tbale starture

---
+++ tables.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class tables
+{
+    public static void main(String args[])
+    {
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=Sc.nextInt();
+
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println(n+"*"+i+"="+(n*i));
+        }
+    }
+}
