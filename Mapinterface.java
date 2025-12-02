a4bff9855ab3969d16c6c08f4957951d4af1942b
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-12-02 15:43:20 +0530

    Mapinterface

---
+++ Mapinterface.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Mapinterface
+{
+    public static void main(String[] args)
+    {
+        Map<String,Integer> Students=new HashMap<String,Integer>();
+        Students.put("Anjali",78);
+        Students.put("Pramila",67);
+        System.out.println(Students);
+
+    }
+}
