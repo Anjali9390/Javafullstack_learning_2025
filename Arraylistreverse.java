bd5929dc879c2be26b925c192dd17b622c16207c
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-12-04 10:27:49 +0530

    Students names printing order

---
+++ Arraylistreverse.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Arraylistreverse
+{
+    public static void main(String[]args)
+    {
+        ArrayList<String> Students=new ArrayList<>();
+        Students.add("rama");
+        Students.add("venky");
+        Students.add("siri");
+
+        System.out.println("college students name"+Students);
+        Collections.reverse(Students);
+        Collections.sort(Students);
+        System.out.println("Alphabetical order list: " + Students);
+
+         System.out.println("\nPrinting elements individually order:");
+
+for(String n:Students)
+{
+       System.out.println(n);
+}
+
+    }
+}
