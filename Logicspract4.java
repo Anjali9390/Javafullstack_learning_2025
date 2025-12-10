8726cb5e706ff24c724a35b6455f782e44900474
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-12-10 09:39:09 +0530

    skip the 3rd table

---
+++ Logicspract4.java
@@ -0,0 +1,14 @@
+public class Logicspract4
+{
+    public static void main(String[] args)
+    {
+        int rows=20;
+        for(int i=1;i<=rows;i++)
+        {
+            if(i%3!=0)
+            {
+                System.out.print(i+"");
+            }
+        }
+    }
+}
