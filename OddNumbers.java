0938faf5a37e1038942e467429ea9de012ec19fd
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-12-11 09:55:42 +0530

    oddnumber

---
+++ OddNumbers.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class OddNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+
+        while(n > 0) {
+            int digit = n % 10;   // extract last digit
+
+            if(digit % 2 != 0) {  // check odd
+                System.out.println("Odd digit: " + digit);
+            }
+
+            n = n / 10;           // remove last digit
+        }
+
+        sc.close();
+    }
+}
