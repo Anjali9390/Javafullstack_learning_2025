245f846c8a90dff5f13ba64ed22612c493851a50
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-12-12 10:33:43 +0530

    REverse triangel

--- ReverseTriangle.java
+++ ReverseTriangle.java
@@ -1,11 +1,15 @@
-
-public class ReverseTriangle{
-    public static void main(String[] args) {
-        for(int i = 5; i >= 1; i--) {   
-            for(int j = 1; j <= i; j++) { 
-                System.out.print("* ");
-            }
-            System.out.println();
+public class ReverseTriangle
+{
+public static void main(String[] args)
+{
+      for(int i=1;i>=5;i--)
+      {
+        for(int j=1;j<=5;j++)
+        {
+            System.out.print("*");
         }
-    }
+      }
+      System.out.println("");
+}
+
 }
