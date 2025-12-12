f7716d34aeb5dc68b2f639f26cc825a1a9088feb
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-12-12 10:26:36 +0530

    rows and colums

---
+++ LogicsPract9.java
@@ -0,0 +1,20 @@
+public class LogicsPract9
+{
+    public static void main(String[]args)
+    {
+        int rows=3;
+        int cols=4;
+        int num=2;
+
+        for(int i=1;i<=rows;i++)
+        {
+            for(int j=1;j<=cols;j++)
+            {
+                    
+                    System.out.print(num+"");
+                   num+=2;
+            }
+            System.out.println("");
+        }
+    }
+}
