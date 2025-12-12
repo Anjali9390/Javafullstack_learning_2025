4017c87c8e051b720d24e6949f7354e025d5d6e4
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-12-12 10:53:52 +0530

    pyramid

---
+++ Pyramid.java
@@ -0,0 +1,18 @@
+public class Pyramid
+{
+    public static void main(String[]args)
+    {
+        for(int i=1;i<=4;i++)
+        {
+            for(int s=1;s<=4-i;s++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i-1);j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
