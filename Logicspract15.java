dc3cdc14f0aba34956d53e9f0edb783494a8c26e
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-12-13 17:42:03 +0530

    odd numbers reverse

---
+++ Logicspract15.java
@@ -0,0 +1,18 @@
+public class Logicspract15
+{
+    public static void main(String[]args)
+    {
+        int n=12345;
+        int rev=0;
+        while(n>0)
+        {
+           int rem=n%10;
+            if(rem%2!=0)
+            {
+                rev=rev*10+rem;
+            }
+            n=n/10;
+        }
+                System.out.println("odd numbers reverse:"+rev);
+    }
+}
