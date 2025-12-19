19042e43aa141a568b74da1f23c7f9fa266baf3c
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-12-19 15:32:50 +0530

    output=1357

---
+++ numberseries.java
@@ -0,0 +1,11 @@
+public class numberseries
+{
+    public static void main(String[]args)
+    {
+        int a=4;
+        for(int i=1;i<=a;i++)
+        {
+             System.out.print((2 * i - 1) + " ");
+        }
+    }
+}
