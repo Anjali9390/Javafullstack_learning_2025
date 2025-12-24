fa56f3903017164e91dc1558d1e4c61c885af7fa
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-12-24 15:28:36 +0530

    Singleton class created only one object

---
+++ SingletonClass.java
@@ -0,0 +1,29 @@
+  class Singleton
+     {
+        private static Singleton obj;
+
+        private Singleton()
+        {
+            System.out.println("Object Created");
+        }
+        public static Singleton getInstance() {
+         if(obj==null)
+         {
+         obj=new Singleton();
+         }
+         return obj;
+        }
+     }
+
+public class SingletonClass
+{
+    public static void main(String[] args) {
+        {
+
+          Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println(s1 == s2); // true
+    }
+    }
+}
